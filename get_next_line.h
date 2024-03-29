1e3d5b19767d437858ef0f95a29627e66d809d4d
Anagamedina <anaga.medina@gmail.com>
2024-03-29 23:08:03 +0100

    iniciando

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 21:38:41 by anamedin          #+#    #+#             */
+/*   Updated: 2024/03/29 21:57:53 by anamedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 35
+# endif
+
+char *get_next_line(int fd);
+size_t ft_strlen(const char *str);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/29 21:58:14 by anamedin          #+#    #+#             */
+/*   Updated: 2024/03/29 22:00:31 by anamedin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t ft_strlen(const char *str)
+{
+  size_t len;
+
+  len = 0;
+  while (str[len])
+	len++
+	  return len;
+}
+    
