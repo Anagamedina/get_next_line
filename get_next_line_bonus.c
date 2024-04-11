55183f0a06749cdd58ec61e269beb09c80b04749
Ana Medina Burgos <anamedin@car13s6.42barcelona.com>
2024-04-11 18:30:14 +0200

    norminete no bonus ok

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 15:00:04 by anamedin          #+#    #+#             */
-/*   Updated: 2024/04/11 16:16:25 by anamedin         ###   ########.fr       */
+/*   Created: 2024/04/11 16:28:35 by anamedin          #+#    #+#             */
+/*   Updated: 2024/04/11 16:45:10 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 static char	*clean_storage(char *storage, char *read_storage, int bytes_read)
 {
@@ -97,19 +97,20 @@ static char	*line_read(char **storage, int fd)
 
 char	*get_next_line(int fd)
 {
-	static char	*storage;
+	static char	*storage[OPEN_MAX];
 	char		*line;
 
 	if ((fd < 0 || BUFFER_SIZE <= 0) || read(fd, 0, 0) < 0)
+		|| fd > OPEN_MAX
 	{
-		return (ft_free(storage), NULL);
+		return (ft_free(storage[fd]), NULL);
 	}
-	storage = initial_buffer(storage);
-	if (storage == NULL)
+	storage[fd] = initial_buffer(storage[fd]);
+	if (storage[fd] == NULL)
 		return (NULL);
-	line = line_read(&storage, fd);
+	line = line_read(&storage[fd], fd);
 	if (line == NULL)
-		return (ft_free(storage), NULL);
+		return (ft_free(storage[fd]), NULL);
 	if (line != NULL && ft_strlen(line) == 0)
 	{
 		ft_free(line);
@@ -118,26 +119,28 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-/*int	main(void)
+int	main(void)
 {
-	int		fd;
+	int		fd[2];
 	char	*line;
 	int		count;
+	int		i;
 
-	//fd = open("test01.txt", O_RDONLY);
-	fd = open("read_error.txt", O_RDONLY);
-	if (fd < 0)
+	i = 0;
+	fd[0] = open("test01.txt", O_RDONLY);
+	fd[1] = open("test02.txt", O_RDONLY);
+	if (fd[i] < 0)
 	{
 		printf("error al abrir el archivo");
 		return (1);
 	}
-	count = 1; // Comenzamos en 1 para que la primera línea sea "LINE [1]"
-	while ((line = get_next_line(fd)) != NULL)
+	count = 1;
+	while ((line = get_next_line(fd[i])) != NULL)
 	{
 		printf("LINE [%d] - %s", count, line);
 		count++;
 		free(line);
 	}
-	close(fd);
+	close(*fd);
 	return (0);
-}*/
+}
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anamedin <anamedin@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/29 21:38:41 by anamedin          #+#    #+#             */
-/*   Updated: 2024/04/11 16:14:23 by anamedin         ###   ########.fr       */
+/*   Created: 2024/04/11 16:29:56 by anamedin          #+#    #+#             */
+/*   Updated: 2024/04/11 18:14:59 by anamedin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10 
+#  define BUFFER_SIZE 10
+#  define OPEN_MAX
+
 # endif
 
 char		*get_next_line(int fd);
